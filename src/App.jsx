4c1b7c2b421ec19a4ce53ae8b84786cc3f57c8d0
Your Name <your.email@example.com>
2024-03-13 11:57:45 -0700

    nasir

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,13 @@ function App() {
       
 
       <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route path='' element={<Home />} />
+        {/* <Route path='/' element={<Layout />} > */}
+        <Route path='/' element={<Home />} />
+
+          <Route path='/' element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='contact/:contactid' element={<Contact />} />
-        </Route>
+        {/* </Route> */}
       </Routes>
 
 {/* through outlet */}
